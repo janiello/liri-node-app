dc428633f9b04a59757a174690b80c024e6b6a25
janiello <janiello417@gmail.com>
2019-05-17 16:53:19 -0400

    Set up liri.js code with initial variables and command prompts. Added a switch case to run specific commands. Initialized axios and created an axios.get function for Bands In Town (ProcessingInstruction is not defined in keys.js)

--- liri.js
+++ liri.js
@@ -1,3 +1,22 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+
+var command = process.argv[2];
+var media = process.argv[3];
+
+switch (command) {
+    case "concert-this":
+        concertThis();
+        break;
+}
+
+var axios = require("axios");
+
+function concertThis() {
+    axios
+    .get("https://rest.bandsintown.com/artists/" + media + "/events?app_id=codingbootcamp")
+    .then(function(response) {
+        console.log(response.data);
+    });
+};
