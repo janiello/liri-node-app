cf00c0fd20f428922c8f709bc78ad13ccff5642f
janiello <janiello417@gmail.com>
2019-05-19 19:16:15 -0400

    Tried to get the spotify and movie commands working with no luck whatsoever. Could not figure out the Spotify API or how to pass my keys to it to search no matter how much I read about it. Accepted failure on this one.

--- liri.js
+++ liri.js
@@ -1,22 +1,41 @@
 require("dotenv").config();
 var keys = require("./keys.js");
+var Spotify = require("node-spotify-api")
 var spotify = new Spotify(keys.spotify);
 
 var command = process.argv[2];
 var media = process.argv[3];
 
 switch (command) {
-    case "concert-this":
-        concertThis();
+    case "spotify-this-song":
+        spotifyThisSong(media);
         break;
-}
+    case "movie-this":
+        movieThis(media);
+};
+
+function spotifyThisSong(song) {
+    spotify.search({type: "track", query: media, limit: 5}), function(error, response) {
+            if (error) {
+                return console.log("Error occurred: " + error);
+            }
+            console.log(response);
+        }
+};
 
 var axios = require("axios");
+var movie = "";
+var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&apikey=" + keys.omdb;
 
-function concertThis() {
-    axios
-    .get("https://rest.bandsintown.com/artists/" + media + "/events?app_id=codingbootcamp")
-    .then(function(response) {
-        console.log(response.data);
+function movieThis(film) {
+    for (var m = 2; m < media.length; m++) {
+        if (m > 2 && m < media.length) {
+            movie = movie + "+" + media[m];
+        } else {
+            movie += media[m];
+        }
+    };
+    axios.get(queryUrl).then(function(response) {
+        console.log(response);
     });
 };
